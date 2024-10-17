c2b86d2e47cf1aa57cb14cd61f7bf65fb5bfacba
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-10-17 22:15:56 +0530

    web socket created

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  path: "/ws",
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("onHelpRequest", (data) => {
+    io.emit("readHelpRequest", data);
+  });
+
+});
+
+server.listen(5500, () => {
+  console.log("server running at port 5500");
+});
