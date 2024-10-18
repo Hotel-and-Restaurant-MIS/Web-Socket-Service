39e0f99603ccf575d37b267a79ede070d0ae71d0
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-10-18 22:14:39 +0530

    customer help message changed.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  socket.on("customerHelpAlert", (data) => {
+  socket.on("RequestCustomerHelp", (data) => {
     io.emit("readHelpRequest", data);
   });
 
